94db812bfe52a87be5cafc92202a8cc2b9d57094
Gilles Perreymond <gperreymond@gmail.com>
2019-09-06 17:11:37 +0200

    feat(): player service on nats

--- RunGame.go
+++ RunGame.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"log"
+	"time"
 
 	Player "github.com/gperreymond/osmo-coding-challenge/models"
+	"github.com/moleculer-go/moleculer"
+	"github.com/moleculer-go/moleculer/broker"
 	"github.com/novalagung/gubrak"
 )
 
 func main() {
+	bkr := broker.New(&moleculer.Config{
+		Transporter: "nats://nats.docker.localhost:4222",
+		LogLevel:    "info",
+		Metrics:     true,
+	})
+	bkr.Start()
+	time.Sleep(time.Millisecond * 1000)
+
 	// Create the players
 	names := []string{"Thrall", "Rexxar", "Gul'Dan", "Malfurion", "Garrosh", "Uther", "Anduin", "Valeera", "Morgl", "Medivh"}
 	players := []Player.Player{}
@@ -15,7 +26,10 @@ func main() {
 		player := Player.Create(name)
 		players = append(players, player)
 		log.Println("player.created", player)
+		result := <-bkr.Call("player.create", "")
+		log.Println(result)
 	}
+
 	// Run a game: Select players and make teams
 	shuffle, _ := gubrak.Shuffle(players)
 	mates := gubrak.RandomInt(3, 5)
@@ -24,5 +38,8 @@ func main() {
 	log.Println("Team Blue: ", teamBlue)
 	teamRed, _ := gubrak.TakeRight(shuffle, mates)
 	log.Println("Team Red: ", teamRed)
+
 	// Run a game: Select players and make teams
+
+	bkr.Stop()
 }
